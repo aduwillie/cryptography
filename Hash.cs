e4b497737e911a2512d8e0994296c123a3ff4118
Williams Adu <aduwillie@gmail.com>
2016-08-22 09:33:52 +0000

    Added hashing algorithms - MD5, SHA256, SHA384, SHA512

---
+++ Hash.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Cryptography;
+
+namespace Cryptography
+{
+    public class Hash
+    {
+        public byte[] ComputeMD5Hash(byte[] toBeHashed)
+        {
+            using(var md5 = MD5.Create())
+            {
+                return md5.ComputeHash(toBeHashed);
+            }
+        }
+
+        public static byte[] ComputeSHA256Hash(byte[] tobeHashed)
+        {
+            using(var sha = SHA256.Create())
+            {
+                return sha.ComputeHash(tobeHashed);
+            }
+        }
+
+        public static byte[] ComputeSHA384Hash(byte[] tobeHashed)
+        {
+            using (var sha = SHA384.Create())
+            {
+                return sha.ComputeHash(tobeHashed);
+            }
+        }
+
+        public static byte[] ComputeSHA512Hash(byte[] tobeHashed)
+        {
+            using (var sha = SHA512.Create())
+            {
+                return sha.ComputeHash(tobeHashed);
+            }
+        }
+    }
+}
