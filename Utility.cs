c15fd51bb9d94d19d837b1a41eec8ba0a08b5c3b
Williams Adu <aduwillie@gmail.com>
2016-08-22 09:25:20 +0000

    Added utility for random number generator and conversions from base 64

---
+++ Utility.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Cryptography
+{
+    public static class Utility
+    {
+        public static byte[] GenerateRandomNumber(int length)
+        {
+            using(var generator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[length];
+                generator.GetBytes(randomNumber);
+                return randomNumber;
+            }
+        }
+
+        public static string ConvertToBase64(byte[] input)
+        {
+            return Convert.ToBase64String(input);
+        }
+
+        public static byte[] ConvertFromBase64(string input)
+        {
+            return Convert.FromBase64String(input);
+        }
+    }
+}
