f29d8922fd145a75b4c61646cb1157244f3d6c16
Williams Adu <aduwillie@gmail.com>
2016-08-22 09:41:16 +0000

    Added hash message authentication code (HMAC) algorithm

---
+++ HMAC.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Cryptography;
+
+namespace Cryptography
+{
+    public class HMAC
+    {
+        public byte[] ComputeHMACMD5(byte[] tobeHashed, byte[] key)
+        {
+            using (var hmac = new HMACMD5(key))
+            {
+                return hmac.ComputeHash(tobeHashed);
+            }
+        }
+
+        public byte[] ComputeHMACSHA256(byte[] tobeHashed, byte[] key)
+        {
+            using (var hmac = new HMACSHA256(key))
+            {
+                return hmac.ComputeHash(tobeHashed);
+            }
+        }
+
+        public byte[] ComputeHMACSHA384(byte[] tobeHashed, byte[] key)
+        {
+            using (var hmac = new HMACSHA384(key))
+            {
+                return hmac.ComputeHash(tobeHashed);
+            }
+        }
+
+        public byte[] ComputeHMACSHA512(byte[] tobeHashed, byte[] key)
+        {
+            using (var hmac = new HMACSHA512(key))
+            {
+                return hmac.ComputeHash(tobeHashed);
+            }
+        }
+    }
+}
